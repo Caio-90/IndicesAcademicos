41fc2afe777eb989b0924bb00486aa53cd8462ed
Caio-90 <caiomachado3a@gmail.com>
2021-11-16 09:33:04 -0300

    back-end pronto

--- Principal/Back/server/json.js
+++ Principal/Back/server/json.js
@@ -17,23 +17,6 @@ const requestListener = function (req, res) {
     res.end(mat);
 });
 }
-function convertMatters(){
-    db.selectMatters().then(matters =>{
-        let values = matters;
-        let i = 5;
-        let mat = '';
-        for (let value of values) {
-            value = JSON.stringify(value)
-            mat = mat + value;
-        }
-        
-        mat = mat.replace(/"/g,'|')
-        //console.log(typeof(mat))
-        return i;
-        
-        }
-        )
-}
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
